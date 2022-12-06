93d6cc5f09c6f4b6ee7f78f5b2a4e60c917994fb
Diogo Costa Hutner <diogohutner@gmail.com>
2022-12-06 08:57:10 -0300

    Explorando conceito do flask Alchemy

--- app.py
+++ app.py
@@ -1,25 +1,72 @@
-from flask import Flask, url_for, redirect, make_response, request
+from flask import Flask, request, url_for, redirect, make_response
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-#app.add_url_rule(rule="/usuario/login",
-#                 view_func=function,
-#                 methods=['POST']
-#                 )
-
-@app.route("/")
-def index():
-    return redirect(url_for('home'))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
+db = SQLAlchemy(app)
 
 
-@app.route("/home")
-def home():
-    return make_response({"response": "Está no home"})
+class students(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    addr = db.Column(db.String(200))
+    pin = db.Column(db.String(10))
+
+    def __init__(self, name, city, addr, pin):
+        self.name = name
+        self.city = city
+        self.addr = addr
+        self.pin = pin
 
 
-@app.errorhandler(404)
-def resource_not_found():
-    return make_response({"error": "Pagina nao encontrada"}, 404)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+
+@app.route("/users")
+def user_list():
+    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    return make_response(users)
+
+
+@app.route("/users/create", methods=["GET", "POST"])
+def user_create():
+    if request.method == "POST":
+        user = User(
+            username=request.form["username"],
+            email=request.form["email"],
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("user_detail", id=user.id))
+
+    return make_response({"Valid": True})
+
+
+@app.route("/user/<int:id>")
+def user_detail(id_detail):
+    user = db.get_or_404(User, id_detail)
+    return make_response(user)
+
+
+@app.route("/user/<int:id>/delete", methods=["GET", "POST"])
+def user_delete(id_delete):
+    user = db.get_or_404(User, id_delete)
+
+    if request.method == "POST":
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for("user_list"))
+
+    return make_response(user)
 
 
 if __name__ == "__main__":
